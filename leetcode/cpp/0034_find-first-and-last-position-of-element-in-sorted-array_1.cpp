bf602e669232d84b18cd34e7eb8a459091cd0151
Alejandro Santos <alejolp@gmail.com>
2020-07-21 00:03:12 +0100

    34

---
+++ leetcode/cpp/0034_find-first-and-last-position-of-element-in-sorted-array_1.cpp
@@ -0,0 +1,29 @@
+int xlower_bound(vector<int>& nums, int target, int start) {
+    int end = nums.size() - 1, med;
+    while (start < end) {
+        med = (end + start) / 2;
+
+        if (target > nums[med]) {
+            start = med + 1;
+        } else {
+            end = med;
+        }
+    }
+    return start;
+}
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.size() == 0) return {-1, -1};
+        int p1 = xlower_bound(nums, target, 0);
+        if (nums[p1] != target) return {-1, -1};
+        int p2 = xlower_bound(nums, target + 1, p1);
+        if ((p2 > 0) && (p2 < nums.size()) && (nums[p2] != target)) {
+            p2--;
+            if (nums[p2] != target) return {-1, -1};
+        }
+        //cout << "p1: " << p1 << " p2: " << p2 << "\n";
+        return {p1, p2};
+    }
+};
